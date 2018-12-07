235ffa06e5df6d5ab5fcb3e02dc7fcc0c113a855
ZaiEf <zaief2@gmail.com>
2018-12-07 16:17:03 +0700

    add mongodb sigleton

---
+++ mongo.go
@@ -0,0 +1,47 @@
+package databases
+
+import (
+	"log"
+
+	"github.com/globalsign/mgo"
+)
+
+type MgoSession struct {
+	Session *mgo.Session
+}
+
+var session MgoSession
+
+func InitMongoDB(url string) {
+	nSess, err := NewMgoSession(url)
+	if err != nil {
+		log.Fatalln("Can't Create Session: ", err)
+	}
+	session = *nSess
+}
+
+func NewMgoSession(url string) (*MgoSession, error) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	return &MgoSession{session}, err
+}
+
+func GetMgoSession() *MgoSession {
+	return &session
+}
+
+func (s *MgoSession) Copy() *MgoSession {
+	return &MgoSession{s.Session.Copy()}
+}
+
+func GetMgoCollection(db, col string) *mgo.Collection {
+	return session.Session.DB(db).C(col)
+}
+
+func (s *MgoSession) Close() {
+	if s.Session != nil {
+		s.Session.Close()
+	}
+}
